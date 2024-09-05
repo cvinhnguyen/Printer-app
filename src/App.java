c0a9cd55f984b676126d7fa0c70f21f83fdeca7d
Vinh Nguyen <amk1001861@student.hamk.fi>
2024-09-05 10:47:02 +0300

    upload-files

---
+++ src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String creator = "Vinh";
+        int number1 = 5;
+        int number2 = 2;
+        int multiplication, addition, division, subtraction;
+
+        System.out.println("Printer App");
+        System.out.print("This app is made by: ");
+        System.out.println(creator);
+
+        System.out.println("The value of number1 is " + number1);
+        System.out.println("The value of number2 is " + number2);
+
+        multiplication = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + multiplication);
+
+        addition = number1 + number2;
+        System.out.println(number1 + " + " + number2 + " = " + addition);
+
+        division = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + division);
+
+        subtraction = number1 - number2;
+        System.out.println(number1 + " - " + number2 + " = " + subtraction);
+    }
+}
